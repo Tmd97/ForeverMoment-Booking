638829362362b15aa78007183fb05fa38af5cfb2
unknown <pri24064@adobe.com>
2026-03-02 15:04:21 +0530

    added hasicorp consul config

--- src/main/java/com/forvmom/MomentForeverBooking/controller/BookingController.java
+++ src/main/java/com/forvmom/MomentForeverBooking/controller/BookingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/admin")
 @Tag(name = "Booking Queries", description = "Endpoints for fetching booking history and details")
 public class BookingController {
 
